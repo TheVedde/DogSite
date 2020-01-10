95d59a8a38f85a9f4c007b345f166040a4f4686d
Vedde <zelenvedran@gmail.com>
2020-01-10 12:25:18 +0100

    Added feature to choose breed in advanced settings

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-var request = new XMLHttpRequest();
+let request = new XMLHttpRequest();
+let dogs;
+let breed = "";
+window.onload=getAllDogs();
 
-request.open('GET', 'https://dog.ceo/api/breeds/list', true);
+function getAllDogs () {
+    let target = "https://dog.ceo/api/breeds/list";
+    request.open('GET', target, true);
+    request.onload = function () {
+        let data = JSON.parse(this.response);
+        dogs = data[Object.keys(data)[0]];
 
-request.onload = function () {
-    let data = JSON.parse(this.response);
+    };
+    request.send();
+}
+
+function f(value) {
+    console.log(value);
+    breed = value;
 
 
-    function myFunction(value) {
-        text += "<li>" + value + "</li>";
+}
+
+function populate() {
+    let text = "";
+   for(let dog of dogs) {
+       text += "<option onclick= \"f('" + dog + "')\"   value=" + dog + ">" + dog + " </option>";
+   }
+
+    document.getElementById("dogSelect").innerHTML = text;
+}
+
+function getRndDog() {
+
+    let target;
+    if(breed === "" || breed == null) {
+       target = "https://dog.ceo/api/breeds/image/random";
+    } else target = "https://dog.ceo/api/breed/" + breed + "/images/random";
+
+    request.open('GET', target, true);
+    request.onload = function () {
+        let data = JSON.parse(this.response);
+        let link = data[Object.keys(data)[0]];
+        let imgSource= "<img src= " + link + " class=\"mx-auto rounded img-thumbnail img-fluid\"alt=\"...\"srcset= " + link + " 500w," + link + " 800vw," + link + " sizes=\" (max-width:500px)80vw,(max-width:750px)10vw,600px\">";
+
+        document.getElementById("dogframe").innerHTML = imgSource
     }
 
-    let dogs = data[Object.keys(data)[0]];
-
-    dogs.forEach(dog => {
-       console.log(dog);
-        text = "<ul>";
-        dogs.forEach(myFunction);
-        text += "</ul>";
-        document.getElementById("root").innerHTML = text;
-    });
-};
-
-
-request.send();
+    request.send();
+}
